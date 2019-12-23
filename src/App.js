c027d347c2caaf3f8bf9ae51fd33ce913c58bb9f
Duviel Garcia <duviel7@gmail.com>
2019-12-23 17:09:03 -0500

    estilos css personalizados, componentes Header, ListadoNoticia, Noticia, llamada a la API, mostar informacion de las noticias

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import Header from './components/Header';
+import ListaNoticias from './components/ListaNoticias';
 
 class App extends Component {
+  state = {
+    noticias: []
+  }
+  componentDidMount() {
+    this.consultarNoticias();  
+  }
+
+  consultarNoticias = async () => {
+    const url =`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=42bf9a0839fc453eb4da7934bb134d63`;
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+
+    this.setState({
+      noticias : noticias.articles
+    });
+  }
+
   render() {
     return (
-      <h1>Noticias API react</h1>
+      <Fragment>
+        <Header titulo={"Noticias React 'News API' "} />
+        <div className="container white contenedor-noticias">
+          <ListaNoticias 
+            noticias={this.state.noticias}
+          />
+        </div>
+      </Fragment>
+      
     );
   }
 }
---
+++ src/components/Noticia.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+const Noticia = ({noticia}) => {
+
+    // extraer los datos
+    const { description, title, url, urlToImage, source } = noticia;
+
+    // mostrar la imagen si esta disponible
+    const imagen = (urlToImage) ?  
+    <div className="card-image">            
+        <img src={urlToImage} alt={title}/>
+        <span className="card-title">{source.name}</span>
+    </div>
+
+    : null;
+
+    return (
+        <div className="col s12 m6 l4">
+            <div className="card">
+                {imagen}
+                        
+                <div className="card-content">
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+                <div className="card-action">
+                    <a 
+                        href={url} 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className="waves-effet waves-light btn">
+                        Ver noticia completa
+                    </a>
+                </div>
+            </div>
+        </div> 
+    );
+};
+
+export default Noticia;
