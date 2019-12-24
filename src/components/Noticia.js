9cbc86b0d482add9c4eaf4a024826b8821b6735d
Duviel Garcia <duviel7@gmail.com>
2019-12-23 19:30:13 -0500

    documentacion de los componentes de la aplicacion mediante los PropTypes

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 const Noticia = ({noticia}) => {
@@ -36,6 +37,10 @@ const Noticia = ({noticia}) => {
             </div>
         </div> 
     );
+};
+
+Noticia.propTypes = {
+    noticia : PropTypes.object.isRequired
 };
 
 export default Noticia;
